36243c8e7e3d80b417c3b6884418f86946d264bc
Sandu Catalin <catalinsandu697@gmail.com>
2026-03-15 23:19:01 +0200

    Initial commit

---
+++ src/test/java/pixel/academy/crud/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.crud.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
