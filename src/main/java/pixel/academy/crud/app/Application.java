9ba3abc3ba7cad4ab4e54bd742a689679d8030e1
Sandu Catalin <catalinsandu697@gmail.com>
2026-03-17 18:49:03 +0200

    Primul lab1

--- src/main/java/pixel/academy/crud/app/Application.java
+++ src/main/java/pixel/academy/crud/app/Application.java
@@ -1,7 +1,10 @@
 package pixel.academy.crud.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,10 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello World");
+        };
+    }
 }
---
+++ src/main/java/pixel/academy/crud/app/dao/StudentDao.java
@@ -0,0 +1,7 @@
+package pixel.academy.crud.app.dao;
+
+import pixel.academy.crud.app.entity.Student;
+
+public interface StudentDao {
+    void safe(Student theStudent);
+}
